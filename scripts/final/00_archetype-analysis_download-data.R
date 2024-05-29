39a2ac7ed5be748307a530bc39f985425fea6e0a
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-28 23:30:47 -0600

    comment out downloads for files I don't use clean up comments at the bottom

--- scripts/final/00_archetype-analysis_download-data.R
+++ scripts/final/00_archetype-analysis_download-data.R
@@ -29,17 +29,12 @@ download_data <- function(url, file_name) {
 #---Download csv data-----------------------------------------------------------
 
 # Partisan sorting, need to download from website
+## Still need way to ref spatially!!
 
-url <- "https://dataverse.harvard.edu/file.xhtml?fileId=8165593&version=3.0#"
-file_name <- "partisan_sorting.csv"
+#url <- "https://dataverse.harvard.edu/file.xhtml?fileId=8165593&version=3.0#"
+#file_name <- "partisan_sorting.csv"
 
-download_data(url, file_name)
-
-# Natural amenity rank
-url <- "https://www.ers.usda.gov/webdocs/DataFiles/52201/natamenf_1_.xls?v=723.9"
-file_name <- "natural_amenity.csv"
-
-download_data(url, file_name)
+#download_data(url, file_name)
 
 # League of Conservation Voters
 rt <- "http://scorecard.lcv.org/exports/"
@@ -54,22 +49,10 @@ for(i in 1:length(yr)){
   download.file(url=link, destfile=fname)
 }
 
-# Economy typology
-url <- "https://www.ers.usda.gov/webdocs/DataFiles/48652/2015CountyTypologyCodes.csv?v=7440.8"
-file_name <- "county_typology_codes_2015.csv"
-
-download_data(url, file_name)
-
 # Change in population
 url <- "https://www.ers.usda.gov/webdocs/DataFiles/48747/PopulationEstimates.csv?v=3995.5"
 file_name <- "population_estimates_2022.csv"
 
-download_data(url, file_name)
-
-# Rural-Urban continuum
-url <- "https://www.ers.usda.gov/webdocs/DataFiles/53251/Ruralurbancontinuumcodes2023.csv?v=4628.7"
-file_name <- "rural_urban_cc_2023.csv"
-  
 download_data(url, file_name)
 
 # Forest dependency 
@@ -82,6 +65,11 @@ download_data(url, file_name)
 x <- getURL("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv")
 y <- read.csv(text = x)
 write.csv(y, here::here("data/original/election_context_2018.csv"))
+
+# Baseline Resilience Indicators for Communities (BRIC) data
+url <- "https://sc.edu/study/colleges_schools/artsandsciences/centers_and_institutes/hvri/documents/bric/bric2020_us_forweb.xlsx"
+
+download.file(url, destfile = here::here("data/original/bric2020_us.xlsx"))
 
 #---Download shp data-----------------------------------------------------------
 
@@ -109,12 +97,12 @@ file_name = ""
 
 download_data(url, file_name)
 
-# FAA Natioanl Tree Growth Database
+# FAA National Tree Growth Database
 url <- "https://www.airporttech.tc.faa.gov/DesktopModules/EasyDNNNews/DocumentDownload.ashx?portalid=0&moduleid=3682&articleid=2870&documentid=3042"
 file_name = "faa_tree-growth-ecoregions.zip"
 
 download_data(url, file_name)
-#unzip(paste0(here::here("data/original/"), file_name),  exdir = here::here("data/original/"))
+unzip(paste0(here::here("data/original/"), file_name),  exdir = here::here("data/original/"))
 
 # Climate and Economic Justice Screening Tool
 url <- "https://static-data-screeningtool.geoplatform.gov/data-versions/1.0/data/score/downloadable/1.0-shapefile-codebook.zip"
@@ -127,47 +115,51 @@ url <- "https://www.fs.usda.gov/rds/archive/products/RDS-2021-0077/RDS-2021-0077
 file_name <- ""
 
 download_data(url, file_name)
+#unzip(paste0(here::here("data/original/"), file_name),  exdir = here::here("data/original/"))
 
 #---Download tif data-----------------------------------------------------------
 ## These may be too large to do through RStudio?
 
-# Elevation
-url <- "https://landfire.gov/bulk/downloadfile.php?FNAME=US_Topo_2020-LF2020_Elev_220_CONUS.zip&TYPE=landfire"
-file_name <- ""
-
-download_data(url, file_name)
-
-# Elevation 
-## from the USGS national map https://apps.nationalmap.gov/3depdem/ export the 
-
 # National Land Cover Data 2021
-url <- "https://s3-us-west-2.amazonaws.com/mrlc/nlcd_2021_land_cover_l48_20230630.zip"
-file_name <- ""
+#url <- "https://s3-us-west-2.amazonaws.com/mrlc/nlcd_2021_land_cover_l48_20230630.zip"
+#file_name <- ""
 
-download_data(url, file_name)
+#download_data(url, file_name)
 
 # Land Cover Change Index
-url <- "https://s3-us-west-2.amazonaws.com/mrlc/nlcd_2001_2021_land_cover_change_index_l48_20230630.zip"
-file_name <- ""
+#url <- "https://s3-us-west-2.amazonaws.com/mrlc/nlcd_2001_2021_land_cover_change_index_l48_20230630.zip"
+#file_name <- ""
 
-download_data(url, file_name)
+#download_data(url, file_name)
 
 # Forest Type
-url <- "https://www.notion.so/CONUS-Forest-Type-1d0c781dd0474cdf82c95015ca7dd5a9?pvs=4"
+url <- "https://data.fs.usda.gov/geodata/rastergateway/forest_type/conus_forest-type.zip"
 file_name <- ""
 
 download_data(url, file_name)
 
-# GPP, NEP, RE
-url <- ""
-file_name <- ""
+#---Data that needs more in-depth downloading, processing-----------------------
+#---is by subscription only or requires an account with AppEARS (EarthData)-----
 
-download_data(url, filename)
-
-#---Data that needs more indepth processing or is by subscription only----------
+# NACP 1-km forest age maps
+# see https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1096
+# see data/original/NA_TreeAge_1096/data 
 
 # Wildfire Risk
-# See script 00_archetype-analysis_download-prep-fire.R
+# See scripts/final/00_archetype-analysis_download-prep-fire.R
 
 # Mill capacity data 
-# See script 00_archetype_analysis_forisk-analysis.R
+# See scripts/final/00_archetype-analysis_forisk-analysis.R
+# Note that raw data is only available to subscribers
+# Change in mill capacity will be in "data/processed" folder
+# millchange_cap.tif. millchangecap_interp*.tif
+
+# Temp, Precip, Elevation, and Travel time 
+# See scripts/final/00_archetype-analysis_geodata_data-prep.R
+
+# Protected Lands and Federal Ownership
+# See scripts/final/00_archetype-analysis_PADUS-data-prep.R
+
+# Forest Gain from Hansen et al 2013 (Global Forest Change) - testing
+# See https://earthenginepartners.appspot.com/science-2013-global-forest/download_v1.1.html
+# See script scripts/test/00_archetype-analysis_forest_gain_data-prep.R
