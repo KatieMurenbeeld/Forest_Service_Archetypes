04f360ff29ffaf8c0fabead76709f7f22e943350
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-28 23:57:07 -0600

    clean comments remove rasterization code testing shannon evenness

--- scripts/final/00_04_archetype-analysis_PADUS_data-prep.R
+++ scripts/final/00_04_archetype-analysis_PADUS_data-prep.R
@@ -50,10 +50,10 @@ identical(st_crs(conus_fedp), st_crs(counties))
 ## Make the multisurface into multipolygons
 conus_fedp <- conus_fedp %>% st_cast("MULTIPOLYGON")
 
-# Turn off using spherical geometry
+## Turn off using spherical geometry
 sf_use_s2(FALSE)
 
-## Check and fix validity
+## Check for invalid geometries and fix
 all(st_is_valid(conus_fedp))
 conus_fedp_val <- st_make_valid(conus_fedp)
 all(st_is_valid(conus_fedp_val))
@@ -64,21 +64,7 @@ all(st_is_empty(conus_fedp_val))
 #conus_fedp_val_noempty = conus_fedp_val[!st_is_empty(conus_fedp_val),]
 #st_is_empty(conus_fedp_val_noempty)
 
-
-#----
-#4. Rasterize the data, may not need to do this until later? Save a shapefile instead?
-## Load the reference raster from fire-data-prep
-#ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
-
-## Set to the same projection
-#conus_fed_val_noempty_p <- st_transform(conus_fedp_val_noempty, crs(ref_rast))
-
-## Rasterize the data and write the raster
-#fed_rast <- rasterize(vect(conus_fed_val_noempty_p), ref_rast, field = "Mang_Name")
-#writeRaster(fed_rast, here::here("data/processed/fed_lands_3000m.tif"), overwrite = TRUE)
-#----
-
-# 5. Calculate the area of overlapping Fed and county polygons (maybe I didn't need to figure out the rasterization yet...)
+# 4. Calculate the area of overlapping Fed and county polygons (maybe I didn't need to figure out the rasterization yet...)
 ## from stack overflow https://gis.stackexchange.com/questions/362466/calculate-percentage-overlap-of-2-sets-of-polygons-in-r
 
 ## Calculate area and tidy up
@@ -104,17 +90,17 @@ counties_sub <- counties %>%
   dplyr::select(GEOID, intersect_area_sum, coverage)
 
 ## save as a shapefile
-#write_sf(obj = counties, dsn = paste0(here::here("data/processed/"), "county_fed_gov_coverage_pct", Sys.Date(), ".shp"), overwrite = TRUE, append = FALSE)
-#print("new shapefile written")
+write_sf(obj = counties, dsn = paste0(here::here("data/processed/"), "county_fed_gov_coverage_pct", Sys.Date(), ".shp"), overwrite = TRUE, append = FALSE)
+print("new shapefile written")
 
-# 6. Calculate the Shannon Diversity Index for Federal ownership by county
+# 5. Calculate the Shannon Diversity Index for Federal ownership by county
 
 ## Calculate area and tidy up
 fed_intersect <- st_intersection(counties_sub, conus_fedp_val) %>% 
   mutate(fed_intersect = st_area(.)) %>% # create new column with shape area
   group_by(GEOID, Mang_Name) %>% # group by GEOID and the Fed agency name
   summarise(fed_inter_sum = sum(fed_intersect)) %>% # sum the intersected areas
-  dplyr::select(GEOID, Mang_Name, fed_inter_sum, intersect_area_sum, coverage) %>%   # only select columns needed to merge
+  dplyr::select(GEOID, Mang_Name, fed_inter_sum) %>%   # only select columns needed to merge
   st_drop_geometry()
   
 # Create a fresh area variable for counties
@@ -123,6 +109,8 @@ counties_join <- mutate(counties, county_area = drop_units(st_area(counties)))
 # Merge by county name
 counties_join <- merge(counties_join, drop_units(fed_intersect), by = "GEOID", all.x = TRUE)
 counties_join$fed_inter_sum[is.na(counties_join$fed_inter_sum)] <- 0
+counties_join <- counties_join %>%
+  dplyr::select(GEOID, Mang_Name, fed_inter_sum, county_area, intersect_area_sum, coverage)
 
 # Calculate Shannon Index and replace NA with 0
 counties_prop <- counties_join %>% 
@@ -139,8 +127,8 @@ counties_shannon <- counties_prop %>%
   group_by(., GEOID) %>% 
   summarise(., numfed = n(), 
             H = sum(step1),
-            fedarea = unique(fed_inter_sum),
-            footprint = unique(county_area),
+            fedarea = fed_inter_sum,
+            footprint = county_area,
             E = H/log((fedarea)))
 counties_shannon$E <- ifelse(drop_units(counties_shannon$fedarea) == 0, 1, counties_shannon$E)
 
