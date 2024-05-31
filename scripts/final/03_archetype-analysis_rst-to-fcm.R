3d09cfcb135038ed52a06dc7e28a05fd996263d2
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-05-31 10:17:32 -0600

    removed forest type reclassify forest ownership

--- scripts/final/03_archetype-analysis_rst-to-fcm.R
+++ scripts/final/03_archetype-analysis_rst-to-fcm.R
@@ -8,10 +8,12 @@ library(patchwork)
 library(geocmeans)
 library(RColorBrewer)
 library(viridis)
+library(raster)
+library()
 
 #---Load the data-----
 ref_rast <- rast(here::here("data/processed/merged/WHP_merge3000m.tif"))
-arch_attri <- rast(here::here("data/processed/arch_attri_2024-05-29.tif"))
+arch_attri <- rast(here::here("data/processed/arch_attri_2024-05-31.tif"))
 mill_change_cap <- rast(here::here("data/processed/millchangecap_interp-2.tif"))
 prec_seas <- rast(here::here("data/processed/prec_seas_3000m.tif"))
 temp_seas <- rast(here::here("data/processed/temp_seas_3000m.tif"))
@@ -21,25 +23,11 @@ for_own <- rast(here::here("data/original/Data/forest_own1/forest_own1.tif"))
 
 ## original rasters
 tree_age <- rast(here::here("data/original/NA_TreeAge_1096/data/conus_age06_1km.tif"))
-for_type <- rast(here::here("data/original/conus_foresttype.img"))
 
 #---Process the original rasters----
 
-## For forest type group into either hardwood or softwood
-hardwood <- c("Oak - Pine Group", "Oak - Hickory Group", "Oak - Gum - Cypress Group", 
-              "Elm - Ash - Cottonwood Group", "Maple - Beech - Birch Group", 
-              "Tropical Hardwoods Group", "Aspen - Birch Group", "Exotic Hardwoos Group", 
-              "Alder - Maple Group", "Wester Oak Group", "Tanoak - Laurel Group", 
-              "Other Western Hardwoods Group")
-softwood <- c("White - Red - Jack Pine Group", "Spruce - Fir Group", "Longleaf - Slash Pine Group",
-              "Loblolly - Shortleaf Pine Group", "Exotic Softwoods Group", "Douglas-fir Group",
-              "Ponderosa Pine Group", "Pinyon - Juniper Group", "Western White Pine Group", 
-              "Fir - Spruce - Mountain Hemlock Group", "Lodgepole Pine Group", 
-              "Hemlock - Sitka Spruce Group", "Western Larch Group", "Redwood Group", 
-              "Other Western Softwoods Group", "California Mixed Conifer Group")
-
 identical(crs(ref_rast), crs(tree_age))
-identical(crs(ref_rast), crs(for_type))
+identical(crs(ref_rast), crs(for_own))
 
 resamp <- function(raster, ref_raster, method){
   rast_proj <- project(raster, crs(ref_raster))
@@ -47,8 +35,18 @@ resamp <- function(raster, ref_raster, method){
 }
 
 tree_age_resamp <- resamp(tree_age, ref_rast, "bilinear")
-for_type_resamp <- resamp(for_type, ref_rast, "bilinear")
 mill_change_resamp <- resamp(mill_change_cap, ref_rast, "bilinear")
+
+# Before resampling forest ownership calculate the percent area that is private
+## reclassify the raster
+### make reclassification matrix
+m <- c(1, 4, 1, 
+       4, 8, 0)
+rclmat <- matrix(m, ncol = 3, byrow = TRUE)
+
+### reclassify using matrix
+for_own_rc <- classify(for_own, rclmat, include.lowest=TRUE)
+
 
 # Check alignment and stack the rasters
 rast_stack <- c(arch_attri, ref_rast, mill_change_resamp, prec_seas, temp_seas, 
