ac79407c33dbbe18a85f06e70724c7f6e0d164f1
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-06-10 09:28:24 -0600

    testing out calculating the % area private forest need to test in a new script and not get this script so dirty

--- scripts/final/03_archetype-analysis_rst-to-fcm.R
+++ scripts/final/03_archetype-analysis_rst-to-fcm.R
@@ -77,7 +77,8 @@ saveRDS(tree_age_agg, file = here::here("data/processed/tree_age_agg.rds"))
 saveRDS(tree_age_resamp, file = here::here("data/processed/tree_age_resamp.rds"))
 
 ####----forest ownership to look at with Matt--------
-for_own
+for_own_proj <- project(for_own, projection)
+saveRDS(for_own_proj, here::here("data/processed/for_own_proj.rds"))
 ## reclassify the raster
 ### make reclassification matrix
 m <- c(1, 4, 1, 
@@ -86,9 +87,12 @@ m <- c(1, 4, 1,
 rclmat <- matrix(m, ncol = 3, byrow = TRUE)
 
 ### reclassify using matrix, make NA = 0
-for_own_rc <- classify(for_own, rclmat, include.lowest=TRUE, others = 0)
+for_own_proj_rc <- classify(for_own_proj, rclmat, include.lowest=TRUE, others = 0)
+saveRDS(for_own_proj_rc, here::here("data/processed/for_own_proj_rc.rds"))
+plot(for_own_proj_rc)
 plot(for_own)
 plot(for_own_rc)
+#for_own_rc_proj <- project(for_own_rc, crs())
 
 # test for idaho
 ## Get list of states in the CONUS
@@ -103,7 +107,7 @@ continental.states <- us.states[us.states$state != "AK" & us.states$state != "HI
 
 ## Download the county boundaries and filter by states
 counties <- tigris::counties(state = continental.states$state, cb = TRUE)
-counties_proj <- counties %>% st_transform(., crs = crs(for_own_rc))
+counties_proj <- counties %>% st_transform(., crs = projection)
 
 ## test for Idaho
 id_counties <- counties_proj %>%
@@ -115,19 +119,14 @@ id_cells_sf <- st_sf(id_cells_sf)
 plot(st_geometry(id_cells_sf))
 plot(st_geometry(id_counties), add = TRUE)
 
-for_own_rc_id <- crop(for_own_rc, st_geometry(id_cells_sf))
-plot(for_own_rc_id)
-
-# from https://gis.stackexchange.com/questions/433375/calculate-area-for-raster-in-r
-# but uses all the memory
-result <- as.data.frame(for_own_rc_id) %>%
-  group_by(forest_own1) %>% 
-  summarise(pixels = n()) %>% 
-  mutate(`area m^2` = pixels * 30 * 30, #multiply by pixel area
-         `area km^2` = round(`area m^2`/1e6,2))
+for_own_proj_rc_id <- crop(for_own_proj_rc, st_geometry(id_cells_sf))
+plot(for_own_proj_rc_id)
+plot(st_geometry(id_cells_sf), add = TRUE)
+plot(id_cells_rst, add = TRUE, alpha = 0.5)
 
 # this is quick but I don't like going from raster to sf and back to raster
 id_cells_frac <- cbind(id_cells_sf, exact_extract(for_own_rc_id, id_cells_sf, "frac"))
+
 ##----Create a template raster for the shapefiles----
 XMIN <- ext(id_cells_sf)$xmin
 XMAX <- ext(id_cells_sf)$xmax
@@ -145,6 +144,24 @@ id_cells_frac_rast <- rasterize(id_cells_frac, id_cells_rst, field = "frac_1")
 # could I just resample since the values are either 0 or 1?
 id_cells_resamp <- resample(for_own_rc_id, id_cells_rst) # I think this is what I want
 # but I need the white areas to be filled with 0s
+#id_cells_resamp_ave <- resample(for_own_rc_id, id_cells_rst, method = "average") 
+
+#for_own_rc_id <- project(for_own_rc_id, "epsg:5070")
+#id_cells_rast <- project(id_cells_rast, "epsg:5070")
+
+id_cells_rst_resamp <- resample(id_cells_rst, for_own_proj_rc_id) # still not quite right
+id_cells_rst_ext <- extend(id_cells_rst, ext(for_own_proj_rc_id))
+id_cells_rst_crop <- crop(id_cells_rst, ext(for_own_rc_id))
+
+id_zonal <- zonal(for_own_proj_rc_id, id_cells_rst_resamp, fun = "mean", as.raster = TRUE, na.rm = TRUE)
+id_zonal_sf <- zonal(for_own_proj_rc_id, vect(id_cells_sf), fun = "mean", as.raster = TRUE, na.rm = TRUE)
+id_zonal_sf
+saveRDS(id_zonal, here::here("data/processed/id_zonal.rds"))
+saveRDS(id_zonal_sf, here::here("data/processed/id_zonal_sf.rds"))
+plot(id_zonal)
+
+# set all NAs to 0 ahead of time
+# before running 
 
 # the reclassified original data 0 = public foresr and 1 = private forest
 plot(for_own_rc_id)
