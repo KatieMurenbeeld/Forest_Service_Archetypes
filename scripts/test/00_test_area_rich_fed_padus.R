89c448af5c6c5ba6af6684f200b4a9d96660c769
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-06-12 09:46:41 -0600

    add the code to run for % fed area and fed richness for CONUS

--- scripts/test/00_test_area_rich_fed_padus.R
+++ scripts/test/00_test_area_rich_fed_padus.R
@@ -128,3 +128,105 @@ plot(st_geometry(nc_proj), add = TRUE)
 
 
 identical(nc_fed_name_rich_rst, intersectFeatures_rich_rst)
+
+
+
+#----Now run for CONUS----
+
+## Load county boundaries from tigris
+counties <- tigris::counties()
+# create the list of CONUS states 
+us.abbr <- unique(fips_codes$state)[1:51]
+us.name <- unique(fips_codes$state_name)[1:51]
+us.fips <- unique(fips_codes$state_code)[1:51]
+
+us.states <- as.data.frame(cbind(us.abbr, us.name, us.fips))
+colnames(us.states) <- c("state", "STATENAME", "FIPS")
+us.states$state <- as.character(us.states$state)
+us.states$STATENAME <- as.character(us.states$STATENAME)
+continental.states <- us.states[us.states$state != "AK" & us.states$state != "HI" & us.states$state != "DC",] #only CONUS
+
+counties <- tigris::counties()
+counties <- counties %>%
+  filter(STATEFP %in% us.states$FIPS) %>%
+  dplyr::select(GEOID, COUNTYFP, STATEFP, geometry)
+
+fed <- st_read(here::here("data/original/PADUS4_0Geodatabase/PADUS4_0_Geodatabase.gdb"), layer = "PADUS4_0Fee") 
+
+conus_fed <- fed %>%
+  filter(State_Nm %in% continental.states) %>%
+  filter(Mang_Type == "FED")
+
+rm(fed)
+
+# reproject
+counties_proj <- st_transform(counties, crs = projection)
+conus_fed_proj <- st_transform(conus_fed, crs = projection)
+conus_fed_proj_name <- conus_fed_proj %>%
+  dplyr::select(Mang_Name)
+conus_fed_proj_type <- conus_fed_proj %>%
+  dplyr::select(Mang_Type)
+
+conus_cells <- st_make_grid(counties_proj, cellsize = 3000)
+conus_cells_sf <- st_sf(conus_cells) 
+# add unique cell id
+conus_cells_sf <- conus_cells_sf %>% 
+  mutate(GRIDCELL_REFID = as.character(row_number()))
+
+## Create a template raster for the shapefiles
+XMIN <- ext(conus_cells_sf)$xmin
+XMAX <- ext(conus_cells_sf)$xmax
+YMIN <- ext(conus_cells_sf)$ymin
+YMAX <- ext(conus_cells_sf)$ymax
+aspectRatio <- (YMAX-YMIN)/(XMAX-XMIN)
+cellSize <- 3000
+NCOLS <- as.integer((XMAX-XMIN)/cellSize)
+NROWS <- as.integer(NCOLS * aspectRatio)
+nc_cells_rst <- rast(ncol=NCOLS, nrow=NROWS, 
+                     xmin=XMIN, xmax=XMAX, ymin=YMIN, ymax=YMAX,
+                     vals=1, crs=projection)
+
+# Calculate the % area of federal land
+conus_fed_type_union <- conus_fed_proj_type %>%
+  st_union(.)
+
+intersections_fedtype <- st_intersects(conus_cells_sf, conus_fed_type_union)
+
+pb <- progress_bar$new(format = "[:bar] :current/:total (:percent)", total = dim(conus_cells_sf)[1])
+
+intersectFeatures_fedtype <- map_dfr(1:dim(nc_cells_sf)[1], function(ix){
+  pb$tick()
+  st_intersection(x = conus_cells_sf[ix,], y = conus_fed_type_union[intersections_fedtype[[ix]],])
+})
+
+hd_intersectFeatures_fedtype <- head(intersections_fedtype, n = xL)
+
+intersectFeatures_fedtype_ar <- hd_intersectFeatures_fedtype %>%
+  mutate(area = st_area(.)) %>%
+  mutate(percent_area = drop_units(area) / (3000*3000))
+
+intersectFeatures_fedtype_ar <- rasterize(intersectFeatures_fedtype_ar, conus_cells_rst, field = "percent_area")
+plot(intersectFeatures_fedtype_ar)
+writeRaster(intersectFeatures_fedtype_ar, here::here(paste0("data/processed/conus_fed_pctarea_", Sys.Date(), ".tif")))
+#plot(st_geometry(counties_proj), add = TRUE)
+
+#----Fed Richness-----
+
+intersections_rich <- st_intersects(conus_cells_sf, conus_fed_proj_name)
+
+pb2 <- progress_bar$new(format = "[:bar] :current/:total (:percent)", total = dim(conus_cells_sf)[1])
+
+intersectFeatures_rich <- map_dfr(1:dim(conus_cells_sf)[1], function(ix){
+  pb2$tick()
+  st_intersection(x = conus_cells_sf[ix,], y = conus_fed_proj_name[intersections_rich[[ix]],])
+})
+
+intersectFeatures_richness <- intersectFeatures_rich %>%
+  group_by(., GRIDCELL_REFID) %>%
+  summarise(., numfed = n())
+
+intersectFeatures_rich_rst <- rasterize(intersectFeatures_richness, conus_cells_rst, field = "numfed")
+plot(intersectFeatures_rich_rst)
+writeRaster(intersectFeatures_rich_rst, here::here(paste0("data/processed/conus_fed_rich_", Sys.Date(), ".tif")))
+
+
