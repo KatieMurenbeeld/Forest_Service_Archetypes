a3696672d499cc8edf45dada804dcc93bf9ee6c9
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-06-12 09:31:28 -0600

    check is richness sfs are identical

--- scripts/test/00_test_area_rich_fed_padus.R
+++ scripts/test/00_test_area_rich_fed_padus.R
@@ -30,14 +30,14 @@ fed_proj_name <- fed_proj %>%
 fed_proj_type <- fed_proj %>%
   dplyr::select(Mang_Type)
 
-nc_cells <- st_make_grid(nc_proj, cellsize = 30000)
+nc_cells <- st_make_grid(nc_proj, cellsize = 3000)
 nc_cells_sf <- st_sf(nc_cells) 
 # add unique cell id
 nc_cells_sf <- nc_cells_sf %>% 
   mutate(GRIDCELL_REFID = as.character(row_number()))
 
-plot(st_geometry(nc_proj))
-plot(st_geometry(nc_cells_sf), add = TRUE)
+#plot(st_geometry(nc_proj))
+#plot(st_geometry(nc_cells_sf), add = TRUE)
 #plot(st_geometry(fed_proj_type), add = TRUE)
 
 #---% area fed----
@@ -50,7 +50,7 @@ XMAX <- ext(nc_cells_sf)$xmax
 YMIN <- ext(nc_cells_sf)$ymin
 YMAX <- ext(nc_cells_sf)$ymax
 aspectRatio <- (YMAX-YMIN)/(XMAX-XMIN)
-cellSize <- 30000
+cellSize <- 3000
 NCOLS <- as.integer((XMAX-XMIN)/cellSize)
 NROWS <- as.integer(NCOLS * aspectRatio)
 nc_cells_rst <- rast(ncol=NCOLS, nrow=NROWS, 
@@ -60,12 +60,12 @@ nc_cells_rst <- rast(ncol=NCOLS, nrow=NROWS,
 nc_fed_type_int <- st_intersection(nc_cells_sf, fed_type_union)
 nc_fed_type_int_ar <- nc_fed_type_int %>%
   mutate(area = st_area(.)) %>%
-  mutate(percent_area = drop_units(area) / (30000*30000))
+  mutate(percent_area = drop_units(area) / (3000*3000))
 
 nc_fedtype_int_ar <- rasterize(nc_fed_type_int_ar, nc_cells_rst, field = "percent_area")
 plot(nc_fedtype_int_ar)
 plot(st_geometry(nc_proj), add = TRUE)
-plot(st_geometry(fed_type_union), add = TRUE)
+#plot(st_geometry(fed_type_union), add = TRUE)
 
 intersections <- st_intersects(nc_cells_sf, fed_type_union)
 
@@ -76,18 +76,18 @@ intersectFeatures <- map_dfr(1:dim(nc_cells_sf)[1], function(ix){
   st_intersection(x = nc_cells_sf[ix,], y = fed_type_union[intersections[[ix]],])
 })
 
-test_intersectFeatures <- head(intersectFeatures, n = 79L)
+test_intersectFeatures <- head(intersectFeatures, n = 2047L)
 
 intersectFeatures_ar <- test_intersectFeatures %>%
   mutate(area = st_area(.)) %>%
-  mutate(percent_area = drop_units(area) / (30000*30000))
+  mutate(percent_area = drop_units(area) / (3000*3000))
 
 identical(intersectFeatures_ar$percent_area, nc_fed_type_int_ar$percent_area)
 
 intersectFeatures_ar_rst <- rasterize(intersectFeatures_ar, nc_cells_rst, field = "percent_area")
 plot(intersectFeatures_ar_rst)
 plot(st_geometry(nc_proj), add = TRUE)
-plot(st_geometry(fed_proj_type), add = TRUE)
+#plot(st_geometry(fed_proj_type), add = TRUE)
 
 identical(intersectFeatures_ar_rst, nc_fedtype_int_ar)
 
@@ -112,12 +112,13 @@ intersectFeatures_rich <- map_dfr(1:dim(nc_cells_sf)[1], function(ix){
   st_intersection(x = nc_cells_sf[ix,], y = fed_proj_name[intersections_rich[[ix]],])
 })
 
-hd_intersectFeatures_rich <- head(intersectFeatures_rich, n = 79L)
+#hd_intersectFeatures_rich <- head(intersectFeatures_rich, n = 79L)
 
 intersectFeatures_richness <- intersectFeatures_rich %>%
   group_by(., GRIDCELL_REFID) %>%
   summarise(., numfed = n())
 
+identical(nc_fed_name_rich, intersectFeatures_richness)
 identical(nc_fed_name_rich$numfed, intersectFeatures_richness$numfed)
 
 intersectFeatures_rich_rst <- rasterize(intersectFeatures_richness, nc_cells_rst, field = "numfed")
