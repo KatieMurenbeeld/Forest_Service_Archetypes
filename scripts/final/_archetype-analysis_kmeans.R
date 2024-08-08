1ecfcc04abe9711fe63ee805e966ca5585742b74
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-08-08 15:47:56 -0600

    code for plot of map

--- scripts/final/_archetype-analysis_kmeans.R
+++ scripts/final/_archetype-analysis_kmeans.R
@@ -17,4 +17,14 @@ km_pmrc_poli <- k_means(rst_fcm_pmrc_poli_sc, centers=8, iter.max = 100, algorit
 plot(km_pmrc_poli)
 
 # Using kmeans() from stats package?
+df <- km_pmrc_poli$lyr1 %>% as.data.frame(xy = TRUE)
 
+kmeans_map <- ggplot() +
+  geom_raster(aes(x = df$x, y = df$y, fill = as.factor(df$lyr1))) +
+  scale_fill_brewer(palette = "Set2") +
+  theme_bw() + 
+  theme(legend.position = "bottom",
+        axis.title.x = element_blank(), 
+        axis.title.y = element_blank())
+
+kmeans_map
