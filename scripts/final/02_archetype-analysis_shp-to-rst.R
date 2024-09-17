87664875f7fd6583d0eb8ff38a6f1c88019bb3f7
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-09-17 12:11:08 -0600

    read in new shapefiles with correct counties and counties filled in. clean up code.

--- scripts/final/02_archetype-analysis_shp-to-rst.R
+++ scripts/final/02_archetype-analysis_shp-to-rst.R
@@ -10,7 +10,8 @@ projection = "epsg:5070"
 #---Load the shapefiles-----
 
 # variables from csv county data
-all_vars <- st_read(here::here("data/processed/all_vars_to_rst_2024-08-12.shp"))
+vars <- st_read(here::here("data/processed/vars_to_rst_2024-09-17.shp"))
+delpop <- st_read(here::here("data/processed/delpop_to_rst_2024-09-17.shp"))
 
 # Wilderness areas
 wild <- st_read(here::here("data/original/S_USA.Wilderness.shp"))
@@ -28,7 +29,7 @@ cejst <- st_read(here::here("data/original/usa.shp"))
 #fed_shann <- st_read(here::here("data/processed/county_fed_shannon_div_even_2024-05-29.shp"))
 
 # fia productivity 
-fia_prod <- st_read(here::here("data/processed/conus_prod_fill_fia_2024-08-12.shp"))
+fia_prod <- st_read(here::here("data/processed/conus_prod_fill_fia_2024-09-12.shp"))
 
 #---Load reference raster----
 ref_rast <- rast(here::here("data/processed/merged/conus_whp_3km_agg_2024-08-09.tif"))
@@ -37,14 +38,17 @@ ref_rast_proj <- project(ref_rast, projection)
 #---Transform the projection of shapefiles----
 st_crs(all_vars)
 
-all_vars_proj <- all_vars %>% st_transform(., crs = projection)
+vars_proj <- vars %>% st_transform(., crs = projection)
+delpop_proj <- delpop %>% st_transform(., crs = projection)
 wild_proj <- wild %>% st_transform(., crs = projection)
 crithab_proj <- crithab %>% st_transform(., crs = projection)
 cejst_proj <- cejst %>% st_transform(., crs = projection)
-#counties_proj <- counties %>% st_transform(., crs = projection)
 fia_proj <- fia_prod %>% st_transform(., crs = projection)
-#fed_cov_proj <- fed_cov %>% st_transform(., crs = projection)
-#fed_shann_proj <- fed_shann %>% st_transform(., crs = projection)
+
+# filter the CEJST for CONUS states
+cejst_proj <- cejst_proj %>%
+  filter(SF != c("Hawaii", "Alaska", "Puerto Rico",
+                  "Northern Mariana Islands", "Guam", "American Samoa"))
 
 #---Calculate distances from wilderness areas and critical habitat
 ## Create a template raster for the shapefiles
@@ -78,54 +82,46 @@ criti_dist_crop <- crop(criti_dist, ref_rast, mask = TRUE)
 #plot(criti_dist_crop)
 names(criti_dist_crop) <- "distance_to_crithab_m"
 
-#---Fill in missing county level data for the CEJST dataset----
-index <- st_touches(all_vars_proj, all_vars_proj)
-
-all_vars_proj_fill <- all_vars_proj %>% 
-  mutate(pct_pay_fill = ifelse(is.na(pct_frp),
-                               apply(index, 1, function(i){mean(.$pct_frp[i], na.rm = TRUE)}),
-                               pct_frp),
-         COMMCAP_fill = ifelse(is.na(COMMCAP),
-                               apply(index, 1, function(i){mean(.$COMMCAP[i], na.rm = TRUE)}),
-                               COMMCAP),
-         R_NET_M_fill = ifelse(is.na(R_NET_M),
-                               apply(index, 1, function(i){mean(.$R_NET_M[i], na.rm = TRUE)}),
-                               R_NET_M)
-         )
+#---Fill in data for the CEJST dataset----
+cejst_index <- st_touches(cejst_proj, cejst_proj)
 
 cejst_proj_fill <- cejst_proj %>%
   mutate(HSEF_fill = ifelse(is.na(HSEF),
-                            apply(index, 1, function(i){mean(.$HSEF[i], na.rm = TRUE)}),
+                            apply(cejst_index, 1, function(i){mean(.$HSEF[i], na.rm = TRUE)}),
                             HSEF),
          HBF_PFS_fill = ifelse(is.na(HBF_PFS),
-                            apply(index, 1, function(i){mean(.$HBF_PFS[i], na.rm = TRUE)}),
+                            apply(cejst_index, 1, function(i){mean(.$HBF_PFS[i], na.rm = TRUE)}),
                             HBF_PFS),
          EBF_PFS_fill = ifelse(is.na(EBF_PFS),
-                            apply(index, 1, function(i){mean(.$EBF_PFS[i], na.rm = TRUE)}),
+                            apply(cejst_index, 1, function(i){mean(.$EBF_PFS[i], na.rm = TRUE)}),
                             EBF_PFS),
          PM25F_PFS_fill = ifelse(is.na(PM25F_PFS),
-                            apply(index, 1, function(i){mean(.$PM25F_PFS[i], na.rm = TRUE)}),
+                            apply(cejst_index, 1, function(i){mean(.$PM25F_PFS[i], na.rm = TRUE)}),
                             PM25F_PFS)
          )
 
+
+
 #---Rasterize shapefiles with variables of interest----
-percent_forpay_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "pct_pay_fill")
-#percent_for_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "pct_frs")
-#percent_ext_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "ext_pct")
-commcap_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "COMMCAP_fill")
-#fordep_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "frst_dp")
-delpop_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "R_NET_M_fill")
-lesshighsch_rast <- rasterize(vect(cejst_proj_fill), ref_rast_proj, field = "HSEF_fill")
-propburd_rast <- rasterize(vect(cejst_proj_fill), ref_rast_proj, field = "HBF_PFS_fill")
-enerburd_rast <- rasterize(vect(cejst_proj_fill), ref_rast_proj, field = "EBF_PFS_fill") 
-pm25_rast <- rasterize(vect(cejst_proj_fill), ref_rast_proj, field = "PM25F_PFS_fill")
+ref_rast_proj_fill <- focal(ref_rast_proj, w = 3, na.rm = TRUE) #fill in the NAs in the ref raster
+
+percent_forpay_rast <- rasterize(vect(vars_proj), templateRas, field = "pct_py_")
+#percent_forpay_rast_focal <- focal(percent_forpay_rast, w = 9, na.rm = TRUE)
+#percent_forpay_rast_crop <- crop(percent_forpay_rast_focal, ref_rast_proj, mask = TRUE)
+#plot(percent_forpay_rast_crop)
+commcap_rast <- rasterize(vect(vars_proj), templateRas, field = "COMMCAP")
+delpop_rast <- rasterize(vect(delpop_proj), templateRas, field = "")
+lesshighsch_rast <- rasterize(vect(cejst_proj_fill), templateRas, field = "HSEF_fill")
+propburd_rast <- rasterize(vect(cejst_proj_fill), templateRas, field = "HBF_PFS_fill")
+enerburd_rast <- rasterize(vect(cejst_proj_fill), templateRas, field = "EBF_PFS_fill") 
+pm25_rast <- rasterize(vect(cejst_proj_fill), templateRas, field = "PM25F_PFS_fill")
 #percent_sitesee_rast <- rasterize(vect(all_vars_proj), ref_rast_proj, field = "sghts_p")
 #percent_govpay_rast <- rasterize(vect(all_vars_proj), ref_rast_proj, field = "gov_p")
 #percent_fed_area_rast <- rasterize(vect(fed_cov_proj), ref_rast_proj, field = "coverag")
 #fed_even_rast <- rasterize(vect(fed_shann_proj), ref_rast_proj, field = "E")
-fia_prod_rast <- rasterize(vect(fia_proj), ref_rast_proj, field = "mn_prd_")
-aip_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "mrp_dlg")
-demshare_rast <- rasterize(vect(all_vars_proj_fill), ref_rast_proj, field = "dmshr_p")
+fia_prod_rast <- rasterize(vect(fia_proj), templateRas, field = "mn_prd_")
+aip_rast <- rasterize(vect(vars_proj), templateRas, field = "mrp_dlgy_f")
+demshare_rast <- rasterize(vect(all_vars_proj_fill), templateRas, field = "dmshr_p")
 
 #---Check alignment and extents-----
 rast_stack <- c(percent_forpay_rast, commcap_rast, delpop_rast, 
@@ -137,4 +133,8 @@ rast_stack <- c(percent_forpay_rast, commcap_rast, delpop_rast,
 plot(rast_stack)
 
 writeRaster(x = rast_stack, filename = paste0(here::here("data/processed/"), "arch_attri_", Sys.Date(), ".tif"), overwrite = TRUE)
+
+
+
+
 
